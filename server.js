451562676f2f14cdb4e919007fa8f310c81e6c44
pham trung <ITCSIU22260@student.hcmiu.edu.vn>
2026-07-13 11:50:07 +0700

    fix page when access website

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.get('/', (req, res) => res.redirect('/login.html'))
 app.use(express.static('public'));
 app.use('/api', matchRoute)
 app.use('/api/auth', authRoute)
